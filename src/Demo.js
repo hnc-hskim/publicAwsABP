ed85250c0a00eab2a2b0ee9e421659f18bb5836c
spcsenti <spcsenti@gmail.com>
2021-10-05 14:22:05 +0900

    commit

--- src/Demo.js
+++ src/Demo.js
@@ -15,52 +15,52 @@ export default class demo extends React.Component {
         this.state = {
            thumbnailImages: [ 
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide1.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide1.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide1.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide1.JPG',
             }, 
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide2.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide2.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide2.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide2.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide3.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide3.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide3.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide3.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide4.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide4.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide4.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide4.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide5.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide5.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide5.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide5.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide6.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide6.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide6.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide6.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide7.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide7.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide7.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide7.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide8.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide8.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide8.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide8.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide9.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide9.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide9.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide9.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide10.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide10.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide10.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide10.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide11.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide11.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide11.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide11.JPG',
             },
             {
-                original: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide12.JPG',
-                thumbnail: 'https://layoutdesigner-thumbnail-bucket.s3.ap-northeast-2.amazonaws.com/Resources/demo/slide12.JPG',
+                original: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide12.JPG',
+                thumbnail: 'https://github.com/hnc-hskim/publicAwsABP/blob/512187804aa3954c5ba5d0d2e8030987db665adf/resources/slide12.JPG',
             },        
           ]
         };
